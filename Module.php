a2c209a284a00d728161d74c00a684907dc87614
Fábio Carneiro <fahecs@gmail.com>
2014-08-20 18:15:53 -0300

    search for the sub namespace in module name
    still needs an update to make it properly work when:
    
    /namespace/subnamespace1/subnamespace2/
    /namespace/subnamespace1/
    
    since both would return "true" in strpos

--- Module.php
+++ Module.php
@@ -8,10 +8,11 @@ class Module
         $e->getApplication()->getEventManager()->getSharedManager()->attach('Zend\Mvc\Controller\AbstractController', 'dispatch', function($e) {
             $controller      = $e->getTarget();
             $controllerClass = get_class($controller);
-            $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
             $config          = $e->getApplication()->getServiceManager()->get('config');
-            if (isset($config['module_layouts'][$moduleNamespace])) {
-                $controller->layout($config['module_layouts'][$moduleNamespace]);
+            foreach($config['module_layouts'] as $module_namespace => $layout){
+                if(strpos($controllerClass, $module_namespace)){
+                    $controller->layout($layout);
+                }
             }
         }, 100);
     }
